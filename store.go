8d3b3e98df0c6744419185517247c5ab031675e1
Ben Tranter <bwtrante@lakeheadu.ca>
2016-02-23 00:59:22 -0500

    First attempt at the Post store API

--- store.go
+++ store.go
@@ -1,6 +1,8 @@
 package lazyblog
 
 import (
+	"bytes"
+	"net/http"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -8,6 +10,49 @@ import (
 
 // DefaultStore @Todo
 var DefaultStore = NewDefaultStore()
+
+// SetPost creates a new post. It will save two versions of the post: a "raw"
+// version, containg the id and the body, and a "rendered" version, containing
+// the id and the rendered and compressed HTML for that post.
+func SetPost(w http.ResponseWriter, post *Post) {
+	// immediately write the response so the client feels snappy
+	// this should be a buffer pool, and response should be gzipped first!
+	var buf bytes.Buffer
+	t.ExecuteTemplate(&buf, "index", post)
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		// not sure what to do with this yet... I guess don't write it to the
+		// store at least?
+	}
+
+	// save to each store. might get some speed gain if you do each inside a
+	// goroutine
+	DefaultStore.Update(func(tx *bolt.Tx) error {
+		raw := tx.Bucket([]byte("raw"))
+		rendered := tx.Bucket([]byte("rendered"))
+
+		err = raw.Put(post.ID, post.Body)
+		if err != nil {
+			return err
+		}
+
+		return rendered.Put(post.ID, buf.Bytes())
+	})
+
+}
+
+// GetPost retrieves the post body given its id, and returns it as a byte
+// slice.
+func GetPost(id []byte) []byte {
+	var buf []byte
+	DefaultStore.View(func(tx *bolt.Tx) error {
+		rendered := tx.Bucket([]byte("rendered")) // i need to make these constants
+		buf = rendered.Get(id)
+		return nil
+	})
+
+	return buf
+}
 
 // NewDefaultStore creates our store if it doesn't already exist. We also
 // create two buckets along with our db: a "raw" bucket, for storing the
