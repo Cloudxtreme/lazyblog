ae52b7c64f4b6ee327e4eeb7c6873f18679f28c8
Ben Tranter <bwtrante@lakeheadu.ca>
2016-02-22 22:28:08 -0500

    Fix spelling mistake

--- handlers.go
+++ handlers.go
@@ -12,6 +12,6 @@ var t = template.Must(template.ParseGlob("templates/*"))
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	err := t.ExecuteTemplate(w, "index", map[string]string{"Title": "Lazy Blog"})
 	if err != nil {
-		log.Fatalln("Coudln't render template for home page!")
+		log.Fatalln("Couldn't render template for home page!")
 	}
 }
