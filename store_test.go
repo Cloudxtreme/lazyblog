c12c912b725107c446dec237d073b813672cce5d
Ben Tranter <bwtrante@lakeheadu.ca>
2016-04-22 21:14:10 -0400

    Use consistent failure messages

--- store_test.go
+++ store_test.go
@@ -7,7 +7,7 @@ import (
 func TestGetID(t *testing.T) {
 	result := GetID([]byte("another-one-56ce5f9a"))
 	if string(result) == "56cef9a" {
-		t.Error("Didn't pass, got %s, expected %s\n", string(result), "56cef9a")
+		t.Error("Failed: got %s, expected %s\n", string(result), "56cef9a")
 	}
 }
 
